1e87f7358e9b38043da96f688fde4d7be3388148
lucasjdc <lucasjpdc@gmail.com>
2025-08-12 22:37:04 -0300

    Add cascade persist and remove to Student-Phone relationship

--- bin/delete-student.php
+++ bin/delete-student.php
@@ -6,7 +6,7 @@ use Alura\Doctrine\Helper\EntityManagerCreator;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $entityManager = EntityManagerCreator::createEntityManager();
-$student = $entityManager->getReference(Student::class, $argv[1]);
+$student = $entityManager->find(Student::class, $argv[1]);
 
 $entityManager->remove($student);
 $entityManager->flush();
--- bin/insert-student.php
+++ bin/insert-student.php
@@ -8,15 +8,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $entityManager = EntityManagerCreator::createEntityManager();
 
-
-$phone1 = new Phone('(93) 99999 - 9999');
-$phone2 = new Phone('(17) 3322 - 1111');
-$entityManager->persist($phone1);
-$entityManager->persist($phone2);
-
 $student = new Student('Aluno com telefones');
-$student->addPhone($phone1);
-$student->addPhone($phone2);
+$student->addPhone(new Phone('(93) 99999 - 9999'));
+$student->addPhone(new Phone('(17) 3322 - 1111'));
 
 $entityManager->persist($student);
 $entityManager->flush();
