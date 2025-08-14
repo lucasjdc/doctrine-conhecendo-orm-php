85779684a679949b87500592dc80a19929ae6153
lucasjdc <lucasjpdc@gmail.com>
2025-08-13 23:28:48 -0300

    Implement enroll-student command and update student display to show courses and phones only if present

--- bin/insert-student.php
+++ bin/insert-student.php
@@ -8,9 +8,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $entityManager = EntityManagerCreator::createEntityManager();
 
-$student = new Student('Aluno com telefones');
-$student->addPhone(new Phone('(93) 99999 - 9999'));
-$student->addPhone(new Phone('(17) 3322 - 1111'));
+$student = new Student($argv[1]);
 
 $entityManager->persist($student);
 $entityManager->flush();
